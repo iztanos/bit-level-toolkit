757c3ed0e100b3e8433b8bbd3b8232453df003c8
itzTanos29 <tanushnry@gmail.com>
2026-03-08 06:21:40 +0000

    Added functions - print_binary(), set_bit(), clear_bit(), toggle_bit()

--- bit_ops.c
+++ bit_ops.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include "bit_ops.h"
 
-void print_binary(unsigned int n){
+// Print binary
+void print_binary(unsigned int n) {
+	int binary_break = 0 ;
 	for(int i=31;i>=0;i--) {
-		printf("%d", (n>>i) & 1);}
-        printf("\n");
+		printf("%d", (n>>i) & 1);
+		
+		binary_break += 1;
+		if (binary_break == 8) {
+			printf(" ");
+			binary_break = 0;
+		}
 	}
+	printf("\n");
+}
+
+// Force a bit to 1
+unsigned int set_bit(unsigned int n, int pos) {
+	return (1u << pos) | n;
+}
+
+// Force a bit to 0
+unsigned int clear_bit(unsigned int n, int pos) {
+	return ~(1u << pos) & n;
+}
+
+// Flip a bit
+unsigned int toggle_bit(unsigned int n, int pos) {
+	return (1u << pos) ^ n;
+}
