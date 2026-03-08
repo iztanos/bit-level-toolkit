378061bd827742e953cc568ebc9fca46d3aa12ed
itzTanos29 <tanushnry@gmail.com>
2026-03-08 06:24:36 +0000

    Added function declarations for print_binary(), set_bit(), clear_bit(), toggle_bit()

--- bit_ops.h
+++ bit_ops.h
@@ -1,6 +1,12 @@
-#ifndef BIT_OPS.H
-#define BIT_OPS.H
+#ifndef BIT_OPS_H
+#define BIT_OPS_H
 
 void print_binary(unsigned int n);
+
+unsigned int set_bit(unsigned int n, int pos);
+
+unsigned int clear_bit(unsigned int n, int pos);
+
+unsigned int toggle_bit(unsigned int n, int pos);
 
 #endif
