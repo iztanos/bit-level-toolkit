3b06be43f94973cf237b46065d354c889f8158a4
itzTanos29 <tanushnry@gmail.com>
2026-03-08 23:37:41 +0000

    Added test case for count and make small improvements to tests

--- tests/test_bit_ops.c
+++ tests/test_bit_ops.c
@@ -9,22 +9,31 @@ void run_tests() {
     print_binary(n);
     
     // Test Set
+    n = 100; // 0110 0100
     unsigned int set = set_bit(n, 3);
     printf("Set bit 3: "); 
     print_binary(set);
     assert(set == 108);
 
     // Test Clear
+    n = 100; // 0110 0100
     unsigned int cleared = clear_bit(n, 2);
     printf("Clear bit 2: "); 
     print_binary(cleared);
     assert(cleared == 96);
 
     // Test Toggle
+    n = 100; // 0110 0100
     unsigned int toggled = toggle_bit(n, 0);
     printf("Toggle bit 0: "); 
     print_binary(toggled);
     assert(toggled == 101);
+
+    // Test Count
+    n = 100; // 0110 0100
+    unsigned int count = count_bits(n);
+    printf("Number of 1 bits: %u\n", count);
+    assert(count == 3);
 }
 
 int main(void) {
