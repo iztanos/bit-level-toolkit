04bfd379f35b33d337aacd9793fc34c83fadb39e
itzTanos29 <tanushnry@gmail.com>
2026-03-09 07:44:17 +0000

    Added CLI support for --print

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "bit_ops.h"
 
 int main(int argc, char* argv[]) {
 	
-	if(argc != 3) {
-		printf("Usage: <number> <position>\n");
-		return 1;
-	}
-	//todo
+	if(argc < 2) {
+        printf("Usage: ./bit-tool <command> [value] [position]\n");
+        return 1;
+    }
+
+    unsigned char* command = argv[1];
+    unsigned int value = atoi(argv[2]);
+    //unsigned int position = atoi(argv[3]);
+
+    if(strcmp(command, "--print") == 0) {
+        print_binary(value);
+    //} else if(strcmp(command, "--set") == 0) {
+        // error check todo
+        //unsigned int results = set_bit(value, position);
+        //print_binary(results);
+    }
 	return 0;
 }
