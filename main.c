330fedf9c5b919deb9b1be447736870fae917441
itzTanos29 <tanushnry@gmail.com>
2026-03-10 00:50:35 +0000

    Added --count, --memory, --endian command

--- main.c
+++ main.c
@@ -43,6 +43,35 @@ int main(int argc, char* argv[]) {
         unsigned int position = (unsigned int)atoi(argv[3]);
         unsigned int results = clear_bit(value, position);
         print_binary(results);
+    } else if(strcmp(command, "--toggle") == 0) {
+        if (argc < 4) {
+            printf("Error: --toggle requires a value and a position\n");
+            printf("Usage: ./bit-tool --toggle [value] [position])\n");
+            return 1;
+        }
+        unsigned int value = (unsigned int)atoi(argv[2]);
+        unsigned int position = (unsigned int)atoi(argv[3]);
+        unsigned int results = toggle_bit(value, position);
+        print_binary(results);
+    } else if(strcmp(command, "--count") == 0) {
+        if (argc < 3) {
+            printf("Error: --count requires a value\n");
+            printf("Usage: ./bit-tool --count [value])\n");
+            return 1;
+        }
+        unsigned int value = (unsigned int)atoi(argv[2]);
+        unsigned int results = count_bits(value);
+        printf("%u\n", results);
+    } else if(strcmp(command, "--memory") == 0) {
+        if (argc < 3) {
+            printf("Error: --memory requires a value\n");
+            printf("Usage: ./bit-tool --toggle [value])\n");
+            return 1;
+        }
+        unsigned int value = (unsigned int)atoi(argv[2]);
+        print_memory(&value, sizeof(value));
+    } else if(strcmp(command, "--endian") == 0) {
+        check_endianness();
     } else {
         printf("Error: Unknown or invalid command\n");
     }
